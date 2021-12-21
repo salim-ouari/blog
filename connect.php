27fa8f3376ce4db290836e5fc0f7079fa4987636
yanis-khiter <yanis.khiter@laplateforme.io>
2021-12-21 12:10:03 +0100

    connect - pdo

--- connect.php
+++ connect.php
@@ -1,8 +1,24 @@
 <?php
 
-// ***********************bdd plesk***********************
+// ********BDD-PLESK****************
 // $bdd = mysqli_connect('localhost', 'salim-ouari2', "Zidane07@", 'salim-ouari_livre-or');
 
-//   **********************bddlocal***********************
+
+// //   *******BDD-LOCAL (PDO) ***************  
+// $bdd = new PDO('mysql:host=localhost;dbname=blog', 'root', '');
+// $bdd->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+// $bdd->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+
+
+
+//   *******BDD-LOCAL (MSQLI) ****************
 $bdd = mysqli_connect('localhost', 'root', '', 'blog');
 mysqli_set_charset($bdd, 'utf8');
+
+
+
+
+
+
+
+
