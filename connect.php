bb3d91991a97609be8194c85ae165470529a263c
salim-ouari <salim.ouari@laplateforme.io>
2021-12-22 09:57:40 +0100

    connect modif

--- connect.php
+++ connect.php
@@ -6,6 +6,9 @@
 
 // //   *******BDD-LOCAL (PDO) ***************  
 // $bdd = new PDO('mysql:host=localhost;dbname=blog', 'root', '');
+
+
+// pour afficher les erreurs
 // $bdd->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 // $bdd->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
 
@@ -14,11 +17,3 @@
 //   *******BDD-LOCAL (MSQLI) ****************
 $bdd = mysqli_connect('localhost', 'root', '', 'blog');
 mysqli_set_charset($bdd, 'utf8');
-
-
-
-
-
-
-
-
