33eee780e1de944cfaacbb0ae31b34003514c2f8
salim-ouari <salim.ouari@laplateforme.io>
2021-12-22 09:53:02 +0100

    ajout footer

--- footer.php
+++ footer.php
@@ -2,3 +2,43 @@
 
 Il contient un accès aux différentes pages. Il doit être inclus dans toutes les
 pages du blog -->
+
+<!-- 3 : Affichage du footer en mode 'déconnecté' -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/81dc42ea59.js" crossorigin="anonymous"></script>
+
+    <title>Document</title>
+</head>
+
+<body>
+    <footer>
+
+        <div class="bloc_footer_nav">
+            <h4 class="h4-nav">Navigation</h4>
+            <ul class="list-nav">
+                <li><a href="../index.php">Accueil</a></li>
+                <li><a href="inscription.php">Inscription</a></li>
+                <li><a href="connexion.php">Connexion</a></li>
+            </ul>
+        </div>
+        <div class="bloc_footer_media">
+            <h4 class="h4">Réseaux sociaux</h4>
+            <ul class="list-icons">
+                <li><a href="https://www.facebook.com/LaPlateformeIO"><i class="fab fa-facebook-square"></i></a></li>
+                <li><a href="https://github.com/salim-ouari/blog"><i class="fab fa-github-square"></i></a></li>
+                <li><a href="https://twitter.com/LaPlateformeIO"><i class="fab fa-twitter-square"></i></a></li>
+                <li><a href="https://www.instagram.com/laplateformeio/?hl=am-et"><i class="fab fa-instagram-square"></i></a></li>
+                <li><a href="https://www.youtube.com/watch?v=a7_WFUlFS94&ab_channel=Fireship"><i class="fab fa-youtube-square"></i></a></li>
+            </ul>
+        </div>
+    </footer>
+    <!-- 3 : Fin de l'affichage du footer en mode 'déconnecté' -->
+</body>
+
+</html>
