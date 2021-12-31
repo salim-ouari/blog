4de0d8094767306d3c899efad2db223edc9319d4
salim-ouari <salim.ouari@laplateforme.io>
2021-12-31 20:07:13 +0100

    modif ajout

--- creer-article.php
+++ creer-article.php
@@ -1,15 +1,8 @@
-<!-- - Une page permettant de créer des articles (creer-article.php) :
-
+<!-- - Une page permettant de créer des articles creer-article.php :
 Cette page possède un formulaire permettant "AUX MODERATEURS" et aux
 "ADMINISTRATEURS" de "CREER DE NOUVEAUX ARTICLES". 
-
 Le formulaire contient donc le texte de l’article, une liste déroulante contenant les catégories existantes
-en base de données et un bouton submit. 
-
-//// Petit resumé personnel
-Le modérateur (42) et l'administrateur (1337) 
-pourrons créer un article qui va s'insérer dans la page des articleS
-(Une page qui va ressembler à la page commentaire du livre d'or)  -->
+<!-- en base de données et un bouton submit.  -->
 
 <?php
 require('connect.php');
@@ -17,28 +10,68 @@ session_start();
 
 if (isset($_SESSION['user']['id_droits']) == 1337 || isset($_SESSION['user']['id_droits']) == 42) {
 
-    // ************sélect la liste des catégories****************
-    $requete = mysqli_query($bdd, "SELECT * FROM 'categories'");
-    $result = mysqli_fetch_all($requete, MYSQLI_ASSOC);
+    $msg = '';
+    $article = $_POST['article'];
+    $id_user = $_SESSION['user']['login'];
+    $cat = $_POST['categorie'];
 
-    if (isset($_GET['submit'])) {
+    // ***************si user appyuie sur bouton submit***************
+    if (isset($_POST['submit'])) {
+
+
+        // *************et si le champ cat et article est correctement rempli**********
+        if (!empty($article) && ($cat)) {
+            // **************insére l'article dans ma base de données*************
+            $requete2 = mysqli_query($bdd, "INSERT INTO articles ('article','id_utilisateur','id_categorie','date') VALUES ('$article', '$id_user',
+            '$cat', NOW())");
+            // $resultat = mysqli_fetch_all($requete2, MYSQLI_ASSOC);
+            echo $msg = 'article bien posté';
+            var_dump($requete2);
+            var_dump($id_user);
+        } else {
+
+            echo $msg = 'veuillez remplir tout les champs';
+        }
     }
+}
+
 ?>
 
-    <!DOCTYPE html>
-    <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>créer article</title>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" />
+    <title>créer article</title>
+</head>
 
-    <body>
+<body>
+    <?php include('header.php'); ?>
 
-    </body>
+    <main>
+        <form action="creer-article.php" method="post">
+            <select name="categorie">
+                <option value="">Choisir une catégorie</option>
+                <option value="1">Cinéma</option>
+                <option value="2">Séries</option>
+                <option value="3">Anim</option>
+            </select>
+            <!-- <input type="text" name="titre" placeholder="titre article">
+                <input type="text" name="description" placeholder="introduction article"> -->
+            <textarea name="article" cols="30" rows="10"></textarea>
+            <input type="submit" name="submit">
+            <?php ?>
+        </form>
+    </main>
+    <?php include('footer.php'); ?>
+</body>
 
-    </html>
+</html>
 
-<?php }
+<?php
+
+
+?>
