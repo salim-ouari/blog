d19489b51adbab5fee5e08887319bb8bded5b56e
salim-ouari <salim.ouari@laplateforme.io>
2021-12-31 20:10:57 +0100

    ajout header

--- header.php
+++ header.php
@@ -16,6 +16,7 @@ administrateur, il peut également accéder à la page admin. Le header doit
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://kit.fontawesome.com/225d5fd287.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
 </head>
 
@@ -26,45 +27,68 @@ administrateur, il peut également accéder à la page admin. Le header doit
 </html>
 <header>
     <div>
-        <h1 id="title">BLOG</h1>
+        <h1 id="title"><span>B</span>LOG</h1>
     </div>
 
-    <nav>
+    <nav id="desktop">
+        <a href="index.php">
+            <i class="fa fa-home" aria-hidden="true"></i>
+            Accueil
+        </a>
+        <?php
+        if (empty($_SESSION)) {
+        ?>
 
-        <ul>
-            <li><a href="index.php">Accueil</a></li>
+            <a href="inscription.php">
+                <i class="fa fa-user-plus" aria-hidden="true"></i>Inscription</a>
 
-            <?php
-            if (empty($_SESSION)) {
-            ?>
-                <li><a href="inscription.php">Inscription</a></li>
-                <li><a href="connexion.php">Connexion</a></li>
+            <a href="connexion.php">
+                <i class="fa fa-user" aria-hidden="true"></i>Connexion</a>
 
-            <?php } else { ?>
+        <?php } else { ?>
+
+            <a href="profil.php">
+                <i class="fa fa-user-circle" aria-hidden="true"></i>Profil</a>
+
+            <a href="deconnect.php">
+                <i class="fas fa-sign-out-alt" aria-hidden="true"></i>Deconnexion</a>
+
+        <?php  }
+
+        ?>
+        <?php if (isset($_SESSION['user']['id_droits']) && $_SESSION['user']['id_droits'] == 1337) { ?>
 
 
-                <li><a href="profil.php">Mon profil</a></li>
-                <li><a href="deconnect.php">Déconnexion</a></li>
+            <!-- <a href="article.php">
+                    <i class="fas fa" aria-hidden="true"></i>Article</a> -->
 
+            <a href="admin.php">
+                <i class="fa fa-user" aria-hidden="true"></i>Admin</a>
 
-            <?php  }
+            <a href="articles.php">
+                <i class="fas fa-book-open" aria-hidden="true"></i>Articles</a>
 
-            ?>
-            <?php if (isset($_SESSION['user']['id_droits']) && $_SESSION['user']['id_droits'] == 1337) { ?>
+        <?php }
+        if (isset($_SESSION['user']['id_droits']) && $_SESSION['user']['id_droits'] != 1) { ?>
+            <a href="creer-article.php">
+                <i class="fas fa-plus-circle" aria-hidden="true"></i>Ajouter Article</a>
 
-                <li><a href="article.php">Article</a></li>
-                <li><a href="admin.php">Admin</a></li>
-                <!-- <li><a href="profil.php">Profil</a></li> -->
-                <li><a href="articles.php">Articles</a></li>
+        <?php } ?>
+        <ul id="firstMenu">
 
-            <?php } ?>
-            <?php if (isset($_SESSION['user']['id_droits']) && $_SESSION['user']['id_droits'] != 1) { ?>
+            <li><i class="fa fa-book" aria-hidden="true"></i>Categories</li>
 
-                <li><a href="creer-article.php">creer article</a></li>
+            <ul id="dropMenu">
+                <li><a href="">Tous</a></li>
 
-            <?php } ?>
+                <li><a href="">Cinéma</a></li>
+
+                <li><a href="">Series</a></li>
+
+                <li><a href="">Anims</a></li>
+            </ul>
 
         </ul>
-
+    </nav>
     </nav>
 </header>
