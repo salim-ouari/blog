a5c5bcc0e13b90c7764e8df53c619385e15c6dad
salim-ouari <salim.ouari@laplateforme.io>
2022-01-05 11:26:19 +0100

    pagination + tri categorie

--- articles.php
+++ articles.php
@@ -1,57 +1,40 @@
 <?php
-require('connect.php');
 session_start();
+//connexion à la base
+require('connect.php');
+//requette pour compter les articles
+$sql_count_articles = mysqli_query($bdd, 'SELECT COUNT(*) AS liste FROM articles');
+$count_articles = mysqli_fetch_all($sql_count_articles, MYSQLI_ASSOC);
 
-//On selectionne la totalité des infos des catégories afin des les afficher dans nos inputs
-$requete_categories = mysqli_query($bdd, "SELECT * FROM categories");
-
-//on récupere le résultat en gardant le nom de nos index
-$result_categories = mysqli_fetch_all($requete_categories, MYSQLI_ASSOC);
-
-//Puis on boucle de l'index du résultat de notre requête afin de créer le nombre d'input équivalent au nombre de catégorie
-$i = 0;
-while (isset($result_categories[$i])) {
-?>
-    <button type="submit" name="categories" value="<?= $result_categories[$i]['id'] ?>"><?= $result_categories[$i]['nom'] ?></button>
-<?php
-    $i++;
+//pagination
+$page = "";
+if (isset($_GET['page'])) {
+    $page = $_GET["page"];
 }
 
-
- //Si une catégorie est définie
- if (isset($_GET['categories'])){
-
-    //Alors on récupere la valeur de l'url et on l'assimile à notre variable 'id de categorie'
-    $id = $_GET['categories'];
-
-
-// ***********requete pour compter les article pour préparer la pagination
-$requete_count_art = mysqli_query($bdd, "SELECT COUNT(*) AS nbre_art FROM articles WHERE id_categorie = '$id'");
-$result_count_art = mysqli_fetch_array($requete_count_art, MYSQLI_ASSOC);
-var_dump($result_count_art);
-// **************************pagination***********************************
-// ************************SI page définie************************
-if (isset($_GET['page'])) {
-
-    $page = $_GET['page'];
-}  // ****************SINON SI 1er fois qu'on charge la page - on affiche pr la 1er fois**************
-else {
-
+if (empty($page)) {
     $page = 1;
 }
-$article_par_page = 5;
-// **********fonction system pour arrondir au chiffre entier**********************
-$nbre_page = ceil($result_count_art[0]["nbre_art"] / $article_par_page);
-$calc_art_page = ($page - 1) * $article_par_page;
+$nbr_article_par_page = 5;
+$nbr_page = ceil($count_articles[0]["liste"] / $nbr_article_par_page);
+$debut = ($page - 1) * $nbr_article_par_page;
 
-// **********requete pr trier les articles*******************************
-$requete_tri_art = mysqli_query($bdd, "SELECT * FROM articles ORDER BY date  DESC LIMIT $calc_art_page , $article_par_page");
-$result_tri_art = mysqli_fetch_all($requete_tri_art, MYSQLI_ASSOC);
-// var_dump($result_tri_art);
 
-if (count($result_tri_art) == 0) {
+//requette pour afficher tous les articles
+$sql_articles = mysqli_query($bdd, "SELECT * FROM articles ORDER BY date DESC LIMIT $debut , $nbr_article_par_page");
+$articles = mysqli_fetch_all($sql_articles, MYSQLI_ASSOC);
+if (count($articles) == 0) {
     header("location: articles.php");
 }
+
+if (isset($_GET['categorie'])) {
+    $page_categorie = $_GET['categorie'];
+}
+
+//requete pour afficher les categories dans le selecteur html
+
+$sql = mysqli_query($bdd, "SELECT categories.* FROM categories ");
+$result_cat = mysqli_fetch_all($sql, MYSQLI_ASSOC);
 
 ?>
 
@@ -62,31 +45,117 @@ if (count($result_tri_art) == 0) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Articles</title>
     <link rel="stylesheet" href="style.css" />
-    <title>Document</title>
+
 </head>
 
 <body>
+    <?php require('header.php') ?>
+    <main class="container">
 
-    <!-- <form action="get">
-        <input type="submit" name="all" value="Tout"></input>
-        <input type="submit" name="cat" value="Cinéma"></input>
-        <input type="submit" name="cat" value="Series"></input>
-        <input type="submit" name="cat" value="Animes"></input>
+        <section class="categorieHidden">
+
+            <form action="" method="GET">
+                <select name="categorie">
+                    <?php foreach ($result_cat as $categorie) { ?>
+                        <option value="<?php echo $categorie['nom']; ?> "><?php echo $categorie['nom']; ?> </option>
+                    <?php } ?>
+                </select>
+
+                <input type='hidden' name='page' value='1'>
+                <button type='submit' name="submit" class="formButton">Valider</button>
+            </form>
+            <div class="pagination">
+                <?php
+                //savoir sur qu'elle page nous sommes 
+                if (isset($_GET['categorie'])) {
+
+                    //requette pour compter les articles
+                    $sql_count_articles_cat = mysqli_query($bdd, "SELECT COUNT(articles.id_categorie) AS liste_cat FROM articles INNER JOIN
+                    categories ON categories.id = articles.id_categorie WHERE categories.nom = '$page_categorie'");
+                    $count_articles_cat = mysqli_fetch_all($sql_count_articles_cat, MYSQLI_ASSOC);
+
+                    $nbr_article_par_page_cat = 5;
+                    $nbr_page_cat = ceil($count_articles_cat[0]["liste_cat"] / $nbr_article_par_page_cat);
+                    $debut_cat = ($page - 1) * $nbr_article_par_page_cat;
 
 
-    </form> -->
-    <input type='hidden' name='page' value='1'>
+                    //requette pour afficher tous les articles
+                    $sql_articles_cat = mysqli_query($bdd, "SELECT * FROM articles INNER JOIN categories ON categories.id = articles.id_categorie
+                    WHERE categories.nom = '$page_categorie' ORDER BY date DESC LIMIT $debut_cat");
 
-    <?php include 'header.php'; ?>
+                    $articles_cat = mysqli_fetch_all($sql_articles_cat, MYSQLI_ASSOC);
 
-    <main style="border:2px solid pink; padding: 30px; height: 50vh; margin-bottom: 5px;">
 
+                    for ($i = 1; $i <= $nbr_page_cat; $i++) {
+                        if ($page != $i)
+                            echo "<a class='page' href='?page=$i&categorie=$page_categorie'>$i</a>&nbsp";
+                        else
+                            echo "<a class='page'>$i</a>&nbsp";
+                    }
+                } else {
+                    for ($i = 1; $i <= $nbr_page; $i++) {
+                        if ($page != $i)
+                            echo "<a class='page' href='?page=$i'>$i</a>&nbsp";
+                        else
+                            echo "<a class='page'>$i</a>&nbsp";
+                    }
+                }
+
+                ?>
+            </div>
+        </section>
+        <section class="conteneur_accueil containerOver">
+
+            <?php
+            //tri par catégorie des articles
+            if (isset($_GET['categorie'])) {
+
+                if (($_GET['categorie']) == $page_categorie) {
+
+                    $sql_categories = mysqli_query($bdd, "SELECT * FROM articles INNER JOIN categories ON categories.id = articles.id_categorie 
+                    WHERE categories.nom = '$page_categorie'");
+
+                    $result = mysqli_fetch_all($sql_categories, MYSQLI_ASSOC);
+                }
+
+                // affichage des articls par catégorie
+                if (isset($_GET['page']) && $_GET['page'] == 1) {
+
+                    for ($i = 0; isset($result[$i]) && $i < 5; $i++) {
+            ?>
+
+                        <div><?= $result[$i]['article'] ?></div>
+                        <div><?= $result[$i]['date'] ?></div>
+                        <div><?php echo '<a href="article.php?id=' . $result[$i]['id'] . '">Détail article</a>'; ?></div>
+
+                    <?php
+                    }
+                } else {
+                    for ($i = 5; isset($result[$i]) && $i < 10; $i++) {
+                    ?>
+                        <div><?= $result[$i]['article'] ?></div>
+                        <div><?= $result[$i]['date'] ?></div>
+                        <div><?php echo '<a href="article.php?id=' . $result[$i]['id'] . '">Détail article</a>'; ?></div>
+
+                    <?php
+                    }
+                }
+            } else {
+                // On boucle sur tous les articles
+                foreach ($articles as $article) {
+                    ?>
+                    <div><?= $article['article'] ?></div>
+                    <div><?= $article['date'] ?></div>
+                    <div><?php echo '<a href="article.php?id=' . $article['id'] . '">Détail article</a>'; ?></div>
+            <?php
+                }
+            }
+            ?>
+        </section>
     </main>
-
-    <?php include 'footer.php'; ?>
-
-
+    <?php  ?>
 </body>
 
 </html>
